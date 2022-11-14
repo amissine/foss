c333a175471f71ef4734328780311c9cb66c5c3d
Alec Missine <alec_missine@yahoo.com>
2022-11-14 17:03:28 -0500

    Add HEX Actors

--- stellar-networks.js
+++ stellar-networks.js
@@ -8,6 +8,10 @@ function stellarNetworks () {
     },
     {
       id: 'public',
+      hex: {
+        agent: 'GCKD37IZDNMZN5ZAYDCJPSJNESQEE7UDPM6C4QU7LPIHFZVN4WPBO2EN',
+        issuer: 'GC7BFT2ZXIQAU2GAYNODPVJV4OBFECV5L3NKO4RV5SHXFUR24M3BZNPY',
+      },
       name: 'PUBLIC',
       passphrase: 'Public Global Stellar Network ; September 2015',
       url: 'https://horizon.stellar.org',

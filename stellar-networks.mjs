95d08e72f1c67b978ada5bf487b0cb6982e93d14
Alec Missine <alec_missine@yahoo.com>
2022-11-20 09:22:24 -0500

    Add hex prop to custom network

--- stellar-networks.mjs
+++ stellar-networks.mjs
@@ -1,7 +1,14 @@
 function stellarNetworks () { // {{{1
-  return [
+  console.log('sdk', window.StellarSdk)
+
+  let networks = [
     { // custom {{{2
       id: 'custom',
+      hex: {
+        agent: '{{ custom.network.hex.agent }}',
+        issuerClawableHexa: '{{ custom.network.hex.issuerClawableHexa }}',
+        issuerHEXA: '{{ custom.network.hex.issuerHEXA }}',
+      },
       name: 'CUSTOM',
       passphrase: 'Test SDF Future Network ; October 2022',
       url: 'https://horizon-futurenet.stellar.org',
@@ -10,7 +17,8 @@ function stellarNetworks () { // {{{1
       id: 'public',
       hex: {
         agent: 'GCKD37IZDNMZN5ZAYDCJPSJNESQEE7UDPM6C4QU7LPIHFZVN4WPBO2EN',
-        issuer: 'GC7BFT2ZXIQAU2GAYNODPVJV4OBFECV5L3NKO4RV5SHXFUR24M3BZNPY',
+        issuerClawableHexa: 'GC7BFT2ZXIQAU2GAYNODPVJV4OBFECV5L3NKO4RV5SHXFUR24M3BZNPY',
+        issuerHEXA: 'GC7BFT2ZXIQAU2GAYNODPVJV4OBFECV5L3NKO4RV5SHXFUR24M3BZNPY',
       },
       name: 'PUBLIC',
       passphrase: 'Public Global Stellar Network ; September 2015',
@@ -20,13 +28,15 @@ function stellarNetworks () { // {{{1
       id: 'test',
       hex: {
         agent: 'GB4Q6UNLMNGQCHC3NKU2OZJBUP7KMELMUDKWNQRG2RA72GSPPBL7USVU',
-        issuer: 'GBVH4OS7KLLY5KTHSO35NB2OUX56Y5QJUSB3BLC47Z6DNLZ4WFOEQA2R',
+        issuerClawableHexa: 'GBVH4OS7KLLY5KTHSO35NB2OUX56Y5QJUSB3BLC47Z6DNLZ4WFOEQA2R',
+        issuerHEXA: 'GBVH4OS7KLLY5KTHSO35NB2OUX56Y5QJUSB3BLC47Z6DNLZ4WFOEQA2R',
       },
       name: 'TESTNET',
       passphrase: 'Test SDF Network ; September 2015',
       url: 'https://horizon-testnet.stellar.org',
     }, // }}}2
-  ];
+  ]
+  return networks;
 }
 
 export { stellarNetworks, } // {{{1

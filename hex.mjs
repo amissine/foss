8855113fa6f4bab7b49e13049ef44360f3c734bd
Alec Missine <alec_missine@yahoo.com>
2023-02-28 08:17:20 -0500

    Add function OfferResults

--- hex.mjs
+++ hex.mjs
@@ -1,6 +1,24 @@
 import { timestamp, } from './utils.mjs' // {{{1
 
-let auxOrderbook = timestamp('Orderbook')
+function OfferResults (tx) { // {{{1
+  this.offerResults = tx.offerResults
+}
+
+OfferResults.prototype.toString = function offerResultsToString () {
+  let or0 = this.offerResults[0]
+  switch (or0.effect) { // TODO: multiple results
+    case 'manageOfferCreated':
+      return ` created id ${or0.currentOffer.offerId}`;
+    case 'manageOfferDeleted':
+      return or0.wasImmediatelyFilled ? 
+        ` amountBought ${or0.amountBought}, amountSold ${or0.amountSold}`
+      : ' deleted.';
+    default:
+      throw new Error(`TODO ${or0.effect}`);
+  }
+}
+
+let auxOrderbook = timestamp('Orderbook') // {{{1
 
 class Orderbook { // {{{1
   constructor (data) { // {{{2
@@ -222,6 +240,7 @@ function offerDeleted (xdr, kind = 'manageBuyOfferResult') { // {{{1
 }
 
 export { // {{{1
-  Orderbook, dog2hexa, hexAssets, hexStartingBalance, hexaValue, hexa2dog, 
+  OfferResults, Orderbook, 
+  dog2hexa, hexAssets, hexStartingBalance, hexaValue, hexa2dog, 
   offerCreated, offerDeleted, 
 }
